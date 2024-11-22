792afa1282d19a631584a3cbb100c0d40cf6bf47
Nathan <56611712+Natsukie-7@users.noreply.github.com>
2024-11-22 09:53:53 -0300

    fix: data alocações (#5)

--- App/Frameworks/Resources/Views/Home.php
+++ App/Frameworks/Resources/Views/Home.php
@@ -4,11 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
-    <h1>Home</h1>
+    <!-- Menu Superior -->
+    <header>
+        <nav>
+            <a href="index.php" class="logo-container">
+                <img src="logoPrinterest.png" alt="Logo do Pinterest" class="logo">
+                <span class="logo-text">Pinterest</span>
+            </a>
+            <div class="menu-buttons">
+                <a href="login.php" class="button red">Entrar</a>
+                <a href="criar_conta.php" class="button gray">Criar Conta</a>
+            </div>
+        </nav>
+    </header>
 
-    <p>Ola <?php print_r($user)  ?> </p>
+    <main class="container">
+        <h1>Bem-vindo ao Pinterest</h1>
+        <p>Explore ideias, inspire-se e comece já!</p>
+    </main>
 </body>
 </html>
